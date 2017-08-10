409fce600cd8ab30f27264cbc43e7f785060f748
pwilmart <wilmarth@ohsu.edu>
2017-08-10 12:58:47 -0700

    minor edits to comments, added license text to Ensembl script

--- Ensembl/reference_proteome_manager_Ensembl.py
+++ Ensembl/reference_proteome_manager_Ensembl.py
@@ -1,4 +1,31 @@
-"""
+"""'reference_proteome_manager_Ensembl.py' written by Delan Huang, OHSU, July 2017.
+
+The MIT License (MIT)
+
+Copyright (c) 2017 OHSU
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Direct questions to:
+Technology & Research Collaborations, Oregon Health & Science University,
+Ph: 503-494-8200, FAX: 503-494-4729, Email: techmgmt@ohsu.edu.
+
 Delan Huang, 2017-07-12
 TODO:
  - More Error Checking (adding/dropping when nothing selected, )
